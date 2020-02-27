d36c6874963a2447ed2682f5fc0b06581d502dcc
BILAL BANGASH <BILALBANGASH@ISB.MTBC.COM>
2020-02-27 13:13:06 +0500

    new h1 tag added

--- src/App.js
+++ src/App.js
@@ -108,6 +108,8 @@ const Example = () => {
     <>
     <h1>Salam Pakistan...........</h1>
     <h1>Welcome to the world of Software Engineers...........</h1>
+    <h1>Welcome to the world of Software Engineers...........</h1>
+
     </>
 
   );
